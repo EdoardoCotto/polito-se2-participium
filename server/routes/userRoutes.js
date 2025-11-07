6f96f1e439689c47f1b859c13e485de80c2504b3
EdoardoCotto <s343449@studenti.polito.it>
2025-11-07 17:14:28 +0100

    createuseradmin

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -51,6 +51,54 @@ const userController = require('../controller/userController');
  */
 router.post('/users', userController.createUser);
 
+/**
+ * @swagger
+ * /users/admin:
+ *   post:
+ *     summary: Create a new user (admin only)
+ *     tags: [Users]
+ *     security:
+ *       - cookieAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required: [username, email, name, surname, password, type]
+ *             properties:
+ *               username:
+ *                 type: string
+ *                 example: newuser
+ *               email:
+ *                 type: string
+ *                 format: email
+ *                 example: newuser@example.com
+ *               name:
+ *                 type: string
+ *                 example: Mario
+ *               surname:
+ *                 type: string
+ *                 example: Rossi
+ *               password:
+ *                 type: string
+ *                 example: securePass123!
+ *               type:
+ *                 type: string
+ *                 enum: [citizen, urban_planner]
+ *                 example: urban_planner
+ *     responses:
+ *       201:
+ *         description: User successfully created by admin
+ *       400:
+ *         description: Validation error
+ *       401:
+ *         description: Not authenticated or not admin
+ *       409:
+ *         description: Username or email already taken
+ */
+router.post('/users/admin', userController.createUserIfAdmin);
+
 module.exports = router;
 
 
